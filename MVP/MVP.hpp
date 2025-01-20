e491977a2ac68d0acc95dee33f8ee97e157fef74
Gabriella Best <gabriellambest@gmail.com>
2025-01-20 14:21:13 -0500

    updated MVP files

--- MVP/MVP.hpp
+++ MVP/MVP.hpp
@@ -1,1 +1,41 @@
+/**
+ * @file MVP.hpp
+ * @author Gabriella Best, gb486
+ *
+ * @brief BLiMS MVP launch variables
+ */
+
+#ifndef MVP_HPP
+#define MVP_HPP
+
 #include <stdint.h>
+
+struct Action
+{
+  float position;
+  uint32_t duration;
+};
+
+namespace MVP
+{
+  namespace constants
+  {
+    extern uint32_t initial_hold_threshold;
+    extern uint32_t turn_hold_threshold;
+    extern uint32_t neutral_hold_threshold;
+    extern float neutral_pos;
+    // action_arr is effectively a cyle of instructions.
+    // [_,0] index = % of motor's possible turning amount it should turn (this amount set in ODrive UI)
+    // [_,1] index = length of pause after each turn
+    extern static Action action_arr[10];
+  }
+  namespace state
+  {
+    float motor_position = 0;
+    int32_t curr_action_duration = 0;
+    int curr_action_index = 0;
+
+  }
+}
+
+#endif
