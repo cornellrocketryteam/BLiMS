8e42e7787969460870ecf6248e9b7d3431e3cb62
Gabriella Best <gabriellambest@gmail.com>
2025-01-26 14:58:12 -0500

    1st compilable BLIMS library. Runs example1.cpp

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -4,3 +4,16 @@
  *
  * @brief example of turning the motor
  */
+
+#include "pico/stdlib.h"
+#include "../src/blims.hpp"
+#include "../src/blims_constants.hpp"
+
+int main()
+{
+  stdio_init_all();
+  BLIMS blims;
+  blims.begin(STANDBY);
+  printf("Success");
+  return 0;
+}
