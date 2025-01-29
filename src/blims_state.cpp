2c55dff9a8bca1e09a85e0a46d8cbfb2a07d9875
Gabriella Best <gabriellambest@gmail.com>
2025-01-29 18:52:54 -0500

    Fixed namespace parent issue

--- src/blims_state.cpp
+++ src/blims_state.cpp
@@ -7,38 +7,40 @@
 
 #include "blims_state.hpp"
 #include "blims_constants.hpp"
-
-namespace flight
+namespace blims
 {
-  bool blims_init = false;
-  BLIMSMode flight_mode = STANDBY;
-  float motor_position = 0;
-  BLIMSDataOut data_out = {
-      .motor_position = 0};
-  float longitude = 0;
-  float latitude = 0;
-  float speed = 0;
-  float track_angle = 0;
-  float heading = 0;
-}
-namespace MVP
-{
-  int32_t curr_action_duration = 0;
-  int curr_action_index = -1;
+  namespace flight
+  {
+    bool blims_init = false;
+    BLIMSMode flight_mode = STANDBY;
+    float motor_position = 0;
+    BLIMSDataOut data_out = {
+        .motor_position = 0};
+    float longitude = 0;
+    float latitude = 0;
+    float speed = 0;
+    float track_angle = 0;
+    float heading = 0;
+  }
+  namespace MVP
+  {
+    int32_t curr_action_duration = 0;
+    int curr_action_index = -1;
 
-  // action_arr is effectively a cyle of instructions.
-  // [_,[0,_]] index = % of motor's possible turning amount to turn (set in ODrive UI)
-  // [_,[_,1]] index = length of pause after each turn
-  Action action_arr[10] = {
-      {0.6f, turn_hold_threshold},
-      {0.5f, neutral_hold_threshold},
-      {0.3f, turn_hold_threshold},
-      {0.5f, neutral_hold_threshold},
-      {0.8f, turn_hold_threshold},
-      {0.5f, neutral_hold_threshold},
-      {0.1f, turn_hold_threshold},
-      {0.5f, neutral_hold_threshold},
-      {1.0f, turn_hold_threshold},
-      {0.5f, neutral_hold_threshold},
-  };
+    // action_arr is effectively a cyle of instructions.
+    // [_,[0,_]] index = % of motor's possible turning amount to turn (set in ODrive UI)
+    // [_,[_,1]] index = length of pause after each turn
+    Action action_arr[10] = {
+        {0.6f, turn_hold_threshold},
+        {0.5f, neutral_hold_threshold},
+        {0.3f, turn_hold_threshold},
+        {0.5f, neutral_hold_threshold},
+        {0.8f, turn_hold_threshold},
+        {0.5f, neutral_hold_threshold},
+        {0.1f, turn_hold_threshold},
+        {0.5f, neutral_hold_threshold},
+        {1.0f, turn_hold_threshold},
+        {0.5f, neutral_hold_threshold},
+    };
+  }
 }
