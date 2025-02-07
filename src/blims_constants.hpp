dd8b77a1ffa6131ead1a444323c7ad7f1cfffa07
Gabriella Best <gabriellambest@gmail.com>
2025-02-07 17:29:33 -0500

    fixed neutral hold bug

--- src/blims_constants.hpp
+++ src/blims_constants.hpp
@@ -11,7 +11,7 @@ constexpr uint16_t wrap_cycle_count = 65535;
 
 //////////MVP Specific Constants//////////
 constexpr uint32_t turn_hold_threshold = 10000;
-constexpr uint32_t neutral_hold_threshold = 75000;
+constexpr uint32_t neutral_hold_threshold = 7500;
 constexpr float neutral_pos = 0.5;
 
 #endif // BLIMS_CONSTANTS_HPP
