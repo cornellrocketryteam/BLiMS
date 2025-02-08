9977566f0937d6ca54b63ab4175ecfa2f3bae3fa
Gabriella Best <gabriellambest@gmail.com>
2025-02-07 22:51:52 -0500

    changed motor pin to 10 for L3

--- src/blims_constants.hpp
+++ src/blims_constants.hpp
@@ -4,7 +4,7 @@
 
 constexpr float brake_alt = 10; // To be updated for when we want BLiMS to brake
 /////NEED TO FIX -  GET FROM BLIMS MODULE
-constexpr float blims_motor = 28; // pin for motor
+constexpr float blims_motor = 10; // pin for motor
 constexpr uint32_t initial_hold_threshold = 20000;
 // want wrap to be as large as possible, increases the amount of steps so that we have as much control as possible
 constexpr uint16_t wrap_cycle_count = 65535;
